966461decc4acf09d3f64350c4d460069670d30e
yuohta819 <thunhan798@gmail.com>
2025-11-01 01:15:36 +0700

    Update OurTeam.vue

--- frontend/src/layout/client/header/ourteam/OurTeam.vue
+++ frontend/src/layout/client/header/ourteam/OurTeam.vue
@@ -1,13 +1,14 @@
 <script setup>
+import Background from '../../detail/detailproduct/Background.vue';
+import BackgroundMiddle from '../../main/middle/BackgroundMiddle.vue';
 import ImgOurTeam from './ImgOurTeam.vue';
-import Background from '../../Detail/DetailProduct/Background.vue';
-import BackgroundMillde from '../../Main/Middle/BackgroundMiddle.vue'
+
 </script>
 <template>
     <Background />
     <div class="sm:px-35 px-10">
         <ImgOurTeam />
     </div>
-    <BackgroundMillde />
+    <BackgroundMiddle  />
 
 </template>
